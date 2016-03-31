467c14d92cc28e3a8197f0662793f3cbbf580ba9
Rupali Tanna <rupalirathod87@gmail.com>
2016-03-30 21:19:26 -0400

    new

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-// SESSIONS
-var session = require('express-session');
-var bcrypt = require('bcrypt');
-var MongoStore = require('connect-mongo')(session)
-
-var authenticateUser = function(username, password, callback) {
-  db.collection('users').findOne({username: username}, function(err, data) {
-    if (err) {throw err;}
-    bcrypt.compare(password, data.password_digest, function(err, passwordsMatch) {
-      if (passwordsMatch) {
-        callback(data);
-      } else {
-        ///
-        callback(false);
-      }
+var MongoClient = require('mongodb').MongoClient;
+mongoURL = 'mongodb://localhost:27017/cards';
+MongoClient.connect(mongoURL, function(err, db){
+  if(!err){
+    db.collection('user').find().toArray(function(err, data){
+      console.log(data);
     })
-  });
-};
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
-app.listen(process.env.PORT || 3000);
+  } else {
+    console.log(err);
+  }
+})

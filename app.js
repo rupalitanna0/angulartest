f0e3dec573e9373046f4be21449a17b16535bf9e
Rupali Tanna <rupalirathod87@gmail.com>
2016-04-01 11:06:24 -0400

    partial routes work

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-var MongoClient = require('mongodb').MongoClient;
-mongoURL = 'mongodb://localhost:27017/cards';
-MongoClient.connect(mongoURL, function(err, db){
-  if(!err){
-    db.collection('user').find().toArray(function(err, data){
-      console.log(data);
-    })
-  } else {
-    console.log(err);
-  }
+var http = require('http');
+var express = require('express')
+var app = express();
+
+http.createServer(app).listen(3000);
+//app.use(express.bodyParser());
+app.get('/home', function(req, res){
+  res.end('index' + req.params.name);
+});
+app.post('/home', function(req, res){
+  res.end('this is a post' + req.body.name) 
 })
+app.get('/newuser', function(req, res){
+  res.end('newuser');
+});
+app.get('/login', function(req, res){
+  res.end('login');
+});
+app.get('/signup', function(req, res){
+  res.end('register');
+});
+app.get('/signup', function(req, res){
+  res.end('register');
+});
+console.log("going through port 3000")

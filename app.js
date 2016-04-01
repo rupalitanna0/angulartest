3b40aeb3e319245a8f832c72913e9947f41665cb
Rupali Tanna <rupalirathod87@gmail.com>
2016-04-01 11:38:54 -0400

    m

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
 var http = require('http');
 var express = require('express')
 var app = express();
+var bodyParser = require('body-parser');//for params
+// SESSIONS
+var session = require('express-session');
+var bcrypt = require('bcrypt');
+var MongoStore = require('connect-mongo')(session);
 
-http.createServer(app).listen(3000);
-//app.use(express.bodyParser());
+var authenticateUser = function(username, password, callback) { //for sessions and login
+  db.collection('users').findOne({username: username}, function(err, data) {
+    if (err) {throw err;}
+    bcrypt.compare(password, data.password_digest, function(err, passwordsMatch) {
+      if (passwordsMatch) {
+        callback(data);
+      } else {
+        ///
+        callback(false);
+      }
+    })
+  });
+};
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));//for frontend library should be downloaded through bower
+
+app.set('view engine', 'ejs');//for views
+
+app.use(bodyParser());
+
+var db;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/invitations';
+MongoClient.connect(mongoUrl, function(err, database) {
+  if (err) { throw err;}
+  db = database;
+  process.on('exit', db.close);
+});
+
+app.use(session({
+  secret: process.env.SESSION_SECRET
+}))
+
+app.get('/', function(req, res){
+  res.send('index');
+});
 app.get('/home', function(req, res){
   res.end('index' + req.params.name);
 });
@@ -23,3 +64,7 @@ app.get('/signup', function(req, res){
   res.end('register');
 });
 console.log("going through port 3000")
+
+
+
+app.listen(process.env.PORT || 3000);
